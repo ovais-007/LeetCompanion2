b82b1e48932bc78d3390166e0b296573e628a037
ovais-007 <ovais.00700@gmail.com>
2025-07-31 09:25:26 +0530

    Initial Commit

---
+++ LeetCodeStats/LeetCodeStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeStatsApp.swift
+//  LeetCodeStats
+//
+//  Created by Ovais on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LeetCodeStatsTests/LeetCodeStatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeStatsTests.swift
+//  LeetCodeStatsTests
+//
+//  Created by Ovais on 31/07/25.
+//
+
+import Testing
+@testable import LeetCodeStats
+
+struct LeetCodeStatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
